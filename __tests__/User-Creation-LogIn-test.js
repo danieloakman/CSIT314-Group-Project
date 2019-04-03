c82ead6df86d3d44540df4232e74dea991108492
Daniel Oakman <daniel.oakman@ezswitch.com.au>
2019-04-03 11:39:11 +1100

    Added Jest test: User-Creation-Login-test.js.

---
+++ __tests__/User-Creation-LogIn-test.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-console */
+const Driver = require('../components/users/Driver');
+const Mechanic = require('../components/users/Mechanic');
+const Admin = require('../components/users/Admin');
+let allUsers = require('../assets/test-files/users');
+const fs = require('fs');
+
+describe('User creation and login tests', () => {
+  jest.useFakeTimers();
+
+  it('User creation', async () => {
+    let driver = new Driver('driver', 'test', 'test@test.com', 'test123');
+    driver.saveUser();
+
+    let mechanic = new Mechanic('mechanic', 'test', 'test2@test.com', 'test123');
+    mechanic.saveUser();
+
+    let admin = new Admin('admin', 'test', 'test3@test.com', 'test123');
+    admin.saveUser();
+
+    console.log(`Successfully created: ${JSON.stringify([driver, mechanic, admin], null, 2)}`);
+
+    // Remove these users:
+    let usersToRemove = [driver.email, mechanic.email, admin.email];
+    allUsers = require('../assets/test-files/users');
+    usersToRemove.forEach(user => {
+      if (allUsers[user]) delete allUsers[user];
+    });
+    fs.writeFileSync('./assets/test-files/users.json', JSON.stringify(allUsers, null, 2), {flag: 'w'});
+    console.log(`Succesfully deleted: ${JSON.stringify(usersToRemove, null, 2)}`);
+  });
+
+  it('Log in', async () => {
+    // todo
+  });
+});
