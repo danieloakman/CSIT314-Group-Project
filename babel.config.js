94b0d67587a78256977de8115065ac9c07f0bcf7
Daniel Nichols <info.danielnichols@gmail.com>
2019-04-06 23:07:30 +1100

    Enable import path aliases
    Helps avoid relative path hell e.g. "../../../../../components/foo.js"

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    env: {
+      development: {
+        plugins: [
+          ['module-resolver',
+            {
+              root: ['./src'
+              ],
+              alias: {
+                '@assets': './assets',
+                '@src': './src',
+                '@components': './src/components'
+              },
+            }
+          ],
+        ],
+      },
+    },
   };
 };
