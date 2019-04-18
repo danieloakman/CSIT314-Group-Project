9df6841c6c9d2f7a8603636e66c0031989baa69c
Daniel Oakman <doakman94@gmail.com>
2019-04-18 21:00:30 +1000

    Added missing npm package and navigation container in AppNavigator.js.

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -3,10 +3,12 @@ import { createAppContainer, createSwitchNavigator, createStackNavigator } from 
 
 import DrawerNavigator from "./DrawerNavigator";
 import SignInCreateAccScreen from "../screens/SignInCreateAccScreen";
+import GMapsTestScreen from "../screens/GMapsTestScreen.js";
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Auth: createStackNavigator({ SignIn: SignInCreateAccScreen }),
-  Main: DrawerNavigator
+  Main: DrawerNavigator,
+  GMapsTest: createStackNavigator({ GMapsTest: GMapsTestScreen })
 }));
