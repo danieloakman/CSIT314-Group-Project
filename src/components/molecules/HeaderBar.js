b9911fa68b3ff04785e146ea8518a4cd349b6ba2
Daniel Nichols <info.danielnichols@gmail.com>
2019-05-10 17:51:39 +1000

    Changes to HeaderBar
    Now shows back button instead of hamburger menu if can go back

--- src/components/molecules/HeaderBar.js
+++ src/components/molecules/HeaderBar.js
@@ -9,10 +9,12 @@ import {
 
 import FlexContainer from "@components/FlexContainer";
 import DrawerButton from "@atoms/DrawerButton";
+import BackButton from "@atoms/BackButton";
+import { withNavigation } from "react-navigation";
 
 const navHeight = 57;
 
-export default class NavHeader extends React.Component {
+class NavHeader extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
@@ -24,13 +26,23 @@ export default class NavHeader extends React.Component {
     return {title: props.title ? props.title : ""};
   }
 
+  _renderDrawerButton () {
+    const index = this.props.navigation.dangerouslyGetParent().state.index;
+    console.log(index);
+    return (
+      index <= 0
+        ? <DrawerButton />
+        : <BackButton />
+    );
+  }
+
   render () {
     return (
       <View style={[styles.container, this.props.style, this.props.withSection ? {borderBottomWidth: 0} : null]} >
         <View style={styles.spacer}/>
         <View style={styles.topNav}>
           <FlexContainer style={[styles.titleComponent, styles.sideButtons]}>
-            {this.props.navLeft ? this.props.navLeft : <DrawerButton />}
+            {this.props.navLeft ? this.props.navLeft : this._renderDrawerButton()}
           </FlexContainer>
           <FlexContainer size={1} style={styles.titleComponent}>
             {this.props.navMid
@@ -45,6 +57,8 @@ export default class NavHeader extends React.Component {
     );
   }
 }
+
+export default withNavigation(NavHeader);
 
 const styles = StyleSheet.create({
   topNav: {
