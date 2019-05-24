796c0c3da811bc82b69b69e4c6faaa04a16bc96e
Daniel Oakman <doakman94@gmail.com>
2019-05-25 00:32:05 +1000

    Revert "tried to split the home screens, no idea if it works or not, can't test." Sorry but it doesn't work. Perhaps make another branch to test this on if you know it may break things while you're working on it.
    This reverts commit 9cbbcad91ddfdf0b63e7cdaff3a36b5f13c276b9.

--- src/components/screens/MechanicHomeScreen.js
+++ src/components/screens/MechanicHomeScreen.js
@@ -13,39 +13,7 @@ import DatabaseService from "@lib/services/DatabaseService";
 import GMapView from "@components/GoogleMapView";
 import {MapView} from "expo";
 
-// used for separating screens
-import RequestListScreen from "@components/screens/MechanicRequestList";
-import RequestViewScreen from "@components/screens/MechanicRequestView";
-
-export default class MechanicHomeScreen extends React.Component {
-  render () {
-    return (
-      <View>
-        <Text style={styles.heading}>Home</Text>
-        <View style={styles.buttons}>
-          <Button
-            title="View Nearby Requests"
-            onPress={() => this.props.navigation.navigate(RequestListScreen) }
-          />
-        </View>
-        <View style={styles.buttons}>
-          <Button
-            title="View Current Offer"
-            onPress={() => this.props.navigation.navigate(RequestViewScreen) }
-          />
-        </View>
-        <View style={styles.buttons}>
-          <Button
-            title="View Active Assistance Request"
-            onPress={() => this.props.navigation.navigate(RequestViewScreen) }
-          />
-        </View>
-      </View>
-    );
-  }
-}
-
-/*
+// todo:
 class MechanicHomeScreen extends React.Component {
   render () {
     return (
@@ -73,6 +41,7 @@ class MechanicHomeScreen extends React.Component {
     );
   }
 }
+
 class RequestList extends React.Component {
   state = {
     user: null,
@@ -130,6 +99,7 @@ class RequestList extends React.Component {
               <Text>Description: {this.state.selectedSR.description}</Text>
             </View>
           }
+          {/* max radius dropdown */}
           <View style={styles.centeredRowContainer}>
             <Text style={styles.textBesideInput}>Max Radius:</Text>
             <View style={{borderWidth: 1, borderRadius: 5}}>
@@ -145,6 +115,7 @@ class RequestList extends React.Component {
               </Picker>
             </View>
           </View>
+          {/* NOTE: haven't tested if disabled works properly */}
           <View style={styles.buttons}>
             <Button
               title="View Request"
@@ -162,6 +133,7 @@ class RequestList extends React.Component {
       </View>
     );
   }
+  /* Note will need to set states first depending on what was clicked */
   _viewRequest () {
     this.props.navigation.navigate("RequestView", {
       selectedSR: this.state.selectedSR,
@@ -170,6 +142,7 @@ class RequestList extends React.Component {
     });
   }
 }
+// if possible, make map to and from
 class RequestView extends React.Component {
   state = {
     offerAmount: null,
@@ -179,6 +152,7 @@ class RequestView extends React.Component {
     offerMade: false,
   }
   componentWillMount () {
+    /* get parameters from the list item which was clicked */
     const { navigation } = this.props;
     this.setState({
       selectedSR: navigation.getParam("selectedSR", "The selected service request"),
@@ -226,6 +200,7 @@ class RequestView extends React.Component {
             }}
           />
         </View>
+        {/* for cancelling requests (not implemented yet) */}
         <Text>Offered Amount: ${this.state.offerAmount}</Text>
         <View style={styles.buttons}>
           <Button
@@ -293,6 +268,7 @@ const MainNavigator = createStackNavigator(
   },
   {
     initialRouteName: "Home",
+    /* header stuff (used for all these screens) */
     defaultNavigationOptions: {
       header: null
     }
@@ -304,7 +280,7 @@ export default class App extends React.Component {
     return <AppContainer />;
   }
 }
-*/
+
 const styles = StyleSheet.create({
   heading: {
     fontSize: 20,
