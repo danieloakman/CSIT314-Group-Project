fb01c632b2647dc89955e86f319dd991365c9ddb
Daniel Nichols <info.danielnichols@gmail.com>
2019-05-27 21:22:53 +1000

    New fuzzy search function
    It's not perfect (or tested), but should allow fuzzy searching within the database, and result weightings for sorting by relevance.

--- src/lib/services/database/core.js
+++ src/lib/services/database/core.js
@@ -1,12 +1,14 @@
 import PouchDB from "pouchdb-react-native";
 import Find from "pouchdb-find";
 import Emitter from "tiny-emitter";
+import Fuse from "fuse.js";
 
 PouchDB.plugin(Find);
 
 class DBConnector {
   static DBs = [];
   constructor (name) {
+    this.dbName = name; // Allow access to database name
     this.db = new PouchDB(name);
     DBConnector.DBs.push(this.db);
 
@@ -40,6 +42,59 @@ class DBConnector {
     for (const db in this.DBs) {
       await db.wipe();
     }
+  }
+
+  /**
+   * Searches database for items starting with a given term and returns an array of weightings and ids. Recommended to debounce for live update searches
+   * @param {string} term The term to search for
+   * @param {object[]} fields The fields in which to search, and optional weightings
+   * @param {string} fields.name The name of the field to search
+   * @param {number} [fields.weight] The field weightings for sorting
+   * @param {boolean} [excludeType] Whether to exclude database name values in each result entry
+   * @return {array}
+   */
+  async fuzzySearch (term, fields, excludeType = false) {
+    /*
+    Note: PouchDB is not a database optimized for partial term searches
+    While it actually can do such a search, it will not use any indexes meaning slow performance. This implementation is an alternative, which may not match in all cases, but should use indexes for better performance.
+    The goal is to perform searches across all data stored, while still maintaining sub-second search times.
+    */
+    let results = {};
+
+    // Search across each field given
+    for (const field of fields) {
+      const match = await this.db.find({
+        selector: {
+          [field.name]: {$gte: term}
+        }
+      });
+      // Create aggregated list of objects
+      match.docs.map((doc) => {
+        results[doc._id] = doc;
+      });
+    }
+
+    const options = {
+      keys: fields,
+      id: "_id",
+      includeScore: true,
+      threshold: 0.6,
+      distance: 100
+    };
+
+    // Perform fuzzy search to apply scores
+    const fuse = new Fuse(results.values(), options);
+    let result = fuse.search(term);
+
+    // Add dbName value to each entry as they may be merged into an array with entries from multiple dbs
+    if (!excludeType) {
+      result = result.map((item) => {
+        item.dbName = this.dbName;
+        return item;
+      });
+    }
+
+    return result;
   }
 }
 
