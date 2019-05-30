49c81b27f6c80d6de6bd816ee01f3d857ec17546
Daniel Nichols <info.danielnichols@gmail.com>
2019-05-30 22:00:55 +1000

    Added service request backend
    Probably incomplete and definitely untested, but it exists

---
+++ src/lib/model/Request.js
@@ -0,0 +1,108 @@
+import RequestDB from "@database/request";
+
+import ModelWithDbConnection from "@model/ModelWithDbConnection";
+
+export default class Request extends ModelWithDbConnection {
+  async init () {
+    this._doc.assignedMechanicID = null;
+    this._doc.offers = [];
+    this._doc.status = "Awaiting offer acceptance";
+    this._doc.creationDate = Date.now();
+    this._doc.completionDate = null;
+  }
+
+  static async getServiceRequest (RequestID) {
+    return RequestDB.getRecord(RequestID);
+  }
+
+  getRequestsInRadius = RequestDB.findInRadius.bind(RequestDB);
+
+  /**
+   * Creates a service request.
+   * The checks to see if the driver and vehicle don't
+   * already have an srId should be done before calling this function.
+   * @param {Object} location
+   * @param {string} driverEmail
+   * @param {string} vehicleId
+   * @param {string} description
+   */
+  static async createServiceRequest (location, driverID, vehicleID, description) {
+    const record = {location, driverID, vehicleID, description};
+    const newRequest = new Request(record);
+    await newRequest.init();
+    RequestDB.createRecord(record);
+    return newRequest.id;
+  }
+
+  static async deleteServiceRequest (RequestID) {
+    await RequestDB.deleteRecord(RequestID);
+  }
+
+  get location () { return this._doc.location; }
+  get DriverID () { return this._doc.DriverID; }
+  get VehicleID () { return this._doc.VehicleID; }
+  get description () { return this._doc.description; }
+  get assignedMechanicID () { return this._doc.assignedMechanicID; }
+  get Offers () { return this._doc.Offers; }
+  get status () { return this._doc.status; }
+  get creationDate () { return new Date(this._doc.creationDate); } // Constructed date objects cannot be stored as is
+  get completionDate () { return new Date(this._doc.completionDate); }
+
+  async setLocation (location) {
+    await RequestDB.updateRecord(this, {location});
+  }
+
+  async setDriverID (driverID) {
+    await RequestDB.updateRecord(this, {driverID});
+  }
+
+  async setVehicleID (vehicleID) {
+    await RequestDB.updateRecord(this, {vehicleID});
+  }
+
+  async setDescription (description) {
+    await RequestDB.updateRecord(this, {description});
+  }
+
+  async setAssignedMechanicID (assignedMechanicID) {
+    await RequestDB.updateRecord(this, {assignedMechanicID});
+  }
+
+  async setOffers (offers) {
+    await RequestDB.updateRecord(this, {offers});
+  }
+
+  async setStatus (status) {
+    await RequestDB.updateRecord(this, {status});
+  }
+
+  /**
+   *
+   * @param {Date} creationDate
+   */
+  async setCreationDate (creationDate) {
+    if (creationDate.constructor === Date) {
+      const date = creationDate.getTime();
+      await RequestDB.updateRecord(this, {date});
+    }
+  }
+
+  /**
+   *
+   * @param {Date} completionDate
+   */
+  async setCompletionDate (completionDate) {
+    if (completionDate.constructor === Date) {
+      const date = completionDate.getTime();
+      await RequestDB.updateRecord(this, {date});
+    }
+  }
+
+  /**
+   * Sets multiple values in the request at once
+   * @param {Object} delta The delta object containing changed values
+   */
+  async setMulti (delta) {
+    await RequestDB.updateRecord(this, delta);
+  }
+}
--- src/lib/services/database/request.js
+++ src/lib/services/database/request.js
@@ -1,25 +1,55 @@
 import DBConnector from "./core";
+import LocationService from "@lib/services/LocationService";
 
 class RequestDB extends DBConnector {
   constructor () {
     super("db.requests");
+    this.db.createIndex({
+      index: {
+        fields: [
+          "location",
+          "location.coords",
+          "location.coords.latitude",
+          "location.coords.longitude",
+          "driverID",
+          "vehicleID",
+          "description",
+          "assignedMechanicID",
+          "offers",
+          "status",
+          "creationDate",
+          "completionDate"
+        ],
+      },
+    });
   }
 
-  getRequest () {
+  /**
+   * Returns a sorted array of service requests (distance in ascending order)
+   * that are within radius distance from location.coords.
+   * @param {locationObject} location
+   * @param {number} radius
+   */
+  async findInRadius (location, radius) {
+    // Get all active requests (Could be improved by querying a reduced area before processing radius, although then the coordinate boundary needs to be accounted for)
+    const activeRequests = await this.db.find({
+      selector: {completionDate: {$exists: true}}
+    }).docs;
 
+    // Return an array of requests in specified radius
+    if (!activeRequests) return [];
+    return activeRequests.map(request => {
+      request.distance = LocationService.getDistanceBetween(location.coords, request.location.coords);
+      return request;
+    })
+      .filter(request => request.distance < radius)
+      .sort((a, b) => a.distance - b.distance);
   }
 
-  createRequest () {
-
-  }
-
-  updateRequest () {
-
-  }
-
-  deleteRequest () {
-
-  }
+  // getRequest = this.getRecord.bind(this);
+  // createRequest = this.createRecord.bind(this);
+  // updateRequest = this.updateRecord.bind(this);
+  // deleteRequest = this.deleteRecord.bind(this);
 }
 
 export default new RequestDB();
